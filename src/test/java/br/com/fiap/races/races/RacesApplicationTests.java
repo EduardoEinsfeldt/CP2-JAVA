6d6199f7c0cef89d543eb810926108f752b468b4
EduardoEinsfeldt <eduardoa.einsfeldt@gmail.com>
2025-04-10 19:26:06 -0300

    inicializando

---
+++ src/test/java/br/com/fiap/races/races/RacesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.races.races;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RacesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
