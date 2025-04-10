176e94dc9034a59c745859ccf22066fa583aa251
EduardoEinsfeldt <eduardoa.einsfeldt@gmail.com>
2025-04-10 20:22:47 -0300

    REPOSITORY

--- src/main/java/br/com/fiap/races/races/repository/RacesRepository.java
+++ src/main/java/br/com/fiap/races/races/repository/RacesRepository.java
@@ -1,4 +1,9 @@
 package br.com.fiap.races.races.repository;
 
-public class RacesRepository {
+import br.com.fiap.races.races.model.Races;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RacesRepository extends JpaRepository<Races, Long> {
 }
--- src/main/java/br/com/fiap/races/races/repository/SkillsRepository.java
+++ src/main/java/br/com/fiap/races/races/repository/SkillsRepository.java
@@ -1,4 +1,9 @@
 package br.com.fiap.races.races.repository;
 
-public class SkillsRepository {
+import br.com.fiap.races.races.model.Skills;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SkillsRepository extends JpaRepository<Skills, Long> {
 }
