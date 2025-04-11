2f98169b950088ff836ba7e9049dd5498932fe2e
EduardoEinsfeldt <eduardoa.einsfeldt@gmail.com>
2025-04-10 21:49:55 -0300

    Fixing Stuff 2.

--- src/main/java/br/com/fiap/races/races/dto/RacesResponse.java
+++ src/main/java/br/com/fiap/races/races/dto/RacesResponse.java
@@ -1,6 +1,8 @@
 package br.com.fiap.races.races.dto;
 
+import br.com.fiap.races.races.model.CorSangue;
 import org.springframework.hateoas.Link;
 
-public record RacesResponse(Long id, String nome, Link link) {
+
+public record RacesResponse(Long id, String nome, int expectativadDeVida, String origem, String descricao, CorSangue corSangue, Link link) {
 }
