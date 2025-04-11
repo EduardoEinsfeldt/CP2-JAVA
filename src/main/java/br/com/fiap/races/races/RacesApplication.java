a6899f92b16f47af6ef550258dd4a410b5b4322f
EduardoEinsfeldt <eduardoa.einsfeldt@gmail.com>
2025-04-11 01:07:41 -0300

    Finishing

--- src/main/java/br/com/fiap/races/races/RacesApplication.java
+++ src/main/java/br/com/fiap/races/races/RacesApplication.java
@@ -1,9 +1,13 @@
 package br.com.fiap.races.races;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info =
+@Info(title = "API de Raças Fictícias", description = "Exemplo de API RESTful com Swagger", version = "v1"))
 public class RacesApplication {
 
 	public static void main(String[] args) {
